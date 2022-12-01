f3bfa5fc8c588e6edb0cda56ae8976de141aa412
Georgia-MAX-holic <97029416+Georgia-MAX-holic@users.noreply.github.com>
2022-12-01 15:57:11 +0900

    Create 백준_1655_가운데를 말하기.py

---
+++ 백준_1655_가운데를 말하기.py
@@ -0,0 +1,25 @@
+import heapq
+import sys
+
+N  =int(sys.stdin.readline())
+
+leftheap = []
+rightheap = []
+
+for i in range(N):
+    num = int(sys.stdin.readline())
+    
+    if len(leftheap) == len(rightheap):
+        heapq.heappush(leftheap, -num)
+        
+    else:
+        heapq.heappush(rightheap,num)
+        
+    if rightheap and rightheap[0] < -leftheap[0]:
+        leftValue = heapq.heappop(leftheap)
+        rightValue = heapq.heappop(rightheap)
+        
+        heapq.heappush(leftheap , -rightValue)
+        heapq.heappush(rightheap, -leftValue)
+        
+    print(-leftheap[0])
